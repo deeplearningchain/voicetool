f0bf723e2fe62ee1e8615a6df1c80a6eca976412
milesparkerr <milesparker832@gmail.com>
2023-03-27 20:17:03 +0000

    update rive head

--- src/components/home content/Head.js
+++ src/components/home content/Head.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import Rive, { Layout, Fit, Alignment } from "@rive-app/react-canvas";
 
 const Head = () => {
   return (
     <section className="flex flex-col gap-10 justify-center items-center">
-      <Image src="/assets/Hypeapes.png" alt="Hypeapes" width={150} height={150} />
+      <Rive
+        src="https://public.rive.app/hosted/139027/44696/ftYE65ulwES-TFJIw9AhIw.riv"
+        layout={new Layout({ fit: Fit.Contain, alignment: Alignment.Center })}
+        width={400}
+        height={400}
+      />
       <button>
         <Image src="/assets/mic.png" alt="Microphone" width={50} height={50} />
       </button>
     </section>
   );
-}
+};
 
-export default Head
+export default Head;
