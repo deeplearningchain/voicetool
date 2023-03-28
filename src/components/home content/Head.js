c04c6224291fa4e4413f5a22e630990106de8a51
milesparkerr <milesparker832@gmail.com>
2023-03-28 15:08:04 +0000

    update rive head

--- src/components/home content/Head.js
+++ src/components/home content/Head.js
@@ -4,7 +4,7 @@ import Rive, { Layout, Fit, Alignment } from "@rive-app/react-canvas";
 
 const Head = (props) => {
   return (
-    <section className="flex flex-col justify-center items-center w-[40rem] h-[30rem]">
+    <section className="flex flex-col justify-center items-center w-[60rem] h-[40rem] relative">
       <Rive
         src="https://public.rive.app/hosted/139027/47193/5wpyMWAKCkK6JvkNVqhvuQ.riv"
         stateMachines="State Machine 1"
@@ -12,7 +12,7 @@ const Head = (props) => {
         artboard="DLC Responsive Animation"
         autoplay={true && props.play}
       />
-      <button>
+      <button className="absolute bottom-[15%]">
         <Image src="/assets/mic.png" alt="Microphone" width={50} height={50} />
       </button>
     </section>
