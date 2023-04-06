52c1d58e33c7f699c34f8be5e5f322ce29ba3be8
milesparkerr <milesparker832@gmail.com>
2023-04-06 14:46:09 +0000

    handle input

--- src/components/home content/ChatBot.js
+++ src/components/home content/ChatBot.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 import Image from "next/image";
 
 const ChatBot = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== "") {
+      // check if input value is not empty or just whitespace
+      setMessages([...messages, inputValue]);
+      setInputValue("");
+    }
+  };
+
   return (
     <section className="text-white flex items-end gap-3 justify-center lg:justify-[unset] mt-10 md:mt-0">
       <Image
@@ -16,25 +32,33 @@ const ChatBot = () => {
           <div className="rounded-[4.73px] border-[0.7px] border-[rgba(255,118,225,0.3)] bg-[rgba(113,58,191,0.3)] focus:outline-none w-[80%] h-fit p-[10px]">
             Hi, I'm dlc, your voice tool!
           </div>
-          {/* this is another message box */}
-          {/* <div className="rounded-[4.73px] border-[0.7px] border-[rgba(255,118,225,0.3)] bg-[rgba(113,58,191,0.3)] focus:outline-none w-[70%] h-fit p-[10px]">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            Nulla consequat massa quis enim
-          </div> */}
+          {/* user message boxes */}
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className="rounded-[4.73px] border-[0.7px] border-[rgba(255,118,225,0.3)] bg-[rgba(113,58,191,0.3)] focus:outline-none w-[70%] h-fit p-[10px]"
+            >
+              {message}
+            </div>
+          ))}
         </article>
 
         {/* input field starts here */}
         <article className="flex h-[29px] gap-2 justify-center">
-          <input className="rounded-[4.73px] border-[0.7px] border-[rgba(255,118,225,0.3)] bg-[rgba(72,21,146,0.3)] focus:outline-none p-1 md:w-[70%] w-[60%]" />
+          <input
+            className="rounded-[4.73px] border-[0.7px] border-[rgba(255,118,225,0.3)] bg-[rgba(72,21,146,0.3)] focus:outline-none p-1 md:w-[70%] w-[60%]"
+            onChange={handleInputChange}
+            value={inputValue}
+            type="text"
+          />
           <button className="bg-[rgba(255,118,225,0.3)] rounded-[4.73px] p-2 flex items-center">
             <Image
               src="/assets/submit.svg"
               alt="Submit Button"
               width={15}
               height={15}
+              onClick={handleButtonClick}
+              type="submit"
             />
           </button>
         </article>
@@ -42,6 +66,6 @@ const ChatBot = () => {
       </div>
     </section>
   );
-}
+};
 
-export default ChatBot
+export default ChatBot;
